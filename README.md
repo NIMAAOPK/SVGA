0fbe6bb2d25b24666a1475d1563df4c2cf7b9483
chenqihao <chenqihao@guangzhoumaitian.com>
2025-08-01 16:47:03 +0800

    修复带音频svga播放失败

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # 通过Glide加载SVGA文件
-# 代码来自[SVGA_Glide_RecyclerView](https://github.com/zzechao/SVGA_Glide_RecyclerView/tree/master)
-更简洁的项目结构
+# 原项目[SVGA_Glide_RecyclerView](https://github.com/zzechao/SVGA_Glide_RecyclerView/tree/master)
+合并原项目代码，删除compiler部分代码
 
 新增了部分kotlin扩展方法
 
-修复了原项目的一些bug
+主要对原项目进行了以下修改：
 
-增强了部分情景下的生命周期管理
+1:修复内存泄漏问题；
+
+2:修复在RecyclerView场景下item回收导致动画播放混乱；
+
+3:优化了部分特殊场景下动画播放的时机，避免了不必要的动画播放；
+
+4:新增对Viewpager+fragment场景生命周期管理demo；
+
--- app/src/main/java/com/en/svga/MainActivity.kt
+++ app/src/main/java/com/en/svga/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import com.opensource.svgaplayer.SVGAParser
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //SVGACache.onCreate(this.applicationContext)
+        SVGAParser.shareParser().init(this)
         supportFragmentManager.beginTransaction()
             .replace(R.id.container_fl, GlideSvgaListFragment(), "ListSvga")
             .commitAllowingStateLoss()
